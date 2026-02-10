dbc195b4c055c2a50720caf46b6f5f7ff2182b1e
Gh0stbreaker <46496187+Gh0stbreaker@users.noreply.github.com>
2026-02-10 17:50:49 +0100

    fix

--- src/AuthServer/Migrations/20260210162706_InitialIdentity1.Designer.cs
+++ src/AuthServer/Migrations/20260210162706_InitialIdentity1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AuthServer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AuthServer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260210162706_InitialIdentity1")]
+    partial class InitialIdentity1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/AuthServer/Migrations/20260210162706_InitialIdentity1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialIdentity1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PreferredLanguage",
+                table: "AspNetUsers",
+                type: "character varying(16)",
+                maxLength: 16,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PreferredLanguage",
+                table: "AspNetUsers");
+        }
+    }
+}
